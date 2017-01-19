43864af07eda43b4b27eda0fc4c3f4838733a00e
chriku <chrikuvellberg@gmail.com>
2017-01-19 15:28:18 +0100

    Mehrere Spalten bei mehre Breite

--- dragview.cpp
+++ dragview.cpp
@@ -1,19 +1,42 @@
 #include "dragview.h"
 #include "blocklist.h"
 
-DragView::DragView(QWidget *parent) : QListWidget(parent)
+DragView::DragView(QWidget *parent) : QTableWidget(parent)
 {
-setDragEnabled(true);
-BlockList list;
-for(int i=0;i<list.blocks.length();i++)
-{
-    QPixmap pix=list.blocks[i]->drawBlock();
-    QListWidgetItem* item=new QListWidgetItem(QIcon(pix),list.blocks[i]->name);
-    addItem(item);
-}
+    setDragEnabled(true);
+    makeCols();
 }
 
 void DragView::dropEvent(QDropEvent *event)
 {
 
+}
+
+void DragView::makeCols()
+{
+    clear();
+    BlockList list;
+    int wid=floor(width()/100.0);
+    setColumnCount(wid);
+    setRowCount((list.blocks.length()/wid)+1);
+    for(int i=0;i<list.blocks.length();i++)
+    {
+        QPixmap pix=list.blocks[i]->drawBlock();
+        QTableWidgetItem* item=new QTableWidgetItem(QIcon(pix),list.blocks[i]->name);
+        int col=i%wid;
+        int row=i/wid;
+        setItem(row,col,item);
+    }
+    int rwidth=width()/wid;
+    rwidth-=3;
+    for(int i=0;i<wid;i++)
+    {
+        setColumnWidth(i,rwidth);
+    }
+}
+
+void DragView::resizeEvent(QResizeEvent *event)
+{
+    QTableWidget::resizeEvent(event);
+    makeCols();
 }
--- dragview.h
+++ dragview.h
@@ -1,14 +1,16 @@
 #ifndef DRAGVIEW_H
 #define DRAGVIEW_H
 
-#include <QListWidget>
+#include <QTableWidget>
 
-class DragView : public QListWidget
+class DragView : public QTableWidget
 {
     Q_OBJECT
 public:
     explicit DragView(QWidget *parent = 0);
 void dropEvent(QDropEvent *event);
+void resizeEvent(QResizeEvent *event);
+void makeCols();
 signals:
 
 public slots:
